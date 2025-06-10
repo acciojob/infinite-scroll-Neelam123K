a748708458cff443974605e373a60cfd8c336953
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2025-06-10 14:39:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here!
+const list = document.getElementId("infi-list");
+let Itemcount = 0;
+function addListItems(count){
+	for(let i = 0; i < count; i++){
+	const li = document.createElement('li');
+    itemCount++;
+    li.textContent = `Item ${itemCount}`;
+    list.appendChild(li);
+	}
+}
+addListItems(10);
 
+window.addEventListener('scroll', () => {
+  const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+  const scrolled = window.scrollY;
+
+  if (Math.ceil(scrolled) >= scrollable) {
+    addListItems(2); 
+  }
+});
